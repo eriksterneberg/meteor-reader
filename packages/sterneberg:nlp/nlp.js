a5ea16096fd939d5f4bf55c404ac14f39dde136c
Erik Sterneberg <eriksterneberg@Eriks-Mac-mini.local>
2015-05-24 10:33:53 +0800

    Refactor

--- packages/sterneberg:nlp/nlp.js
+++ packages/sterneberg:nlp/nlp.js
@@ -1,13 +1,17 @@
+var STRIP_PUNCTUATION_BASE_REGEX = "[',.;:!\?！。，：；<>]+",
+    STRIP_SUFFIX_REGEX = new RegExp(STRIP_PUNCTUATION_BASE_REGEX + '$', 'g'),
+    STRIP_PREFIX_REGEX = new RegExp('^' + STRIP_PUNCTUATION_BASE_REGEX, 'g');
+
 var _stripPunctuation = function(text) {
     return _stripPunctuationSuffix(_stripPunctuationPrefix(text));
 };
 
 var _stripPunctuationSuffix = function(text) {
-    return text.replace(/[',.;:!\?！。，：；<>]+$/g, '');
+    return text.replace(STRIP_SUFFIX_REGEX, '');
 };
 
 var _stripPunctuationPrefix = function(text) {
-    return text.replace(/^[',.;:!！\?。，：；<>]+/g, '');
+    return text.replace(STRIP_PREFIX_REGEX, '');
 };
 
 Nlp = {
