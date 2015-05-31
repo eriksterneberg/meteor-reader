6c0a19a0d127b129530c12cdc52675fd96cf8f49
Erik Sterneberg <erik.sterneberg@gmail.com>
2015-05-31 15:58:35 +0800

    Now looks up words regardless of case

--- packages/esperantodict/publications.js
+++ packages/esperantodict/publications.js
@@ -1,3 +1,9 @@
 Meteor.publish('eo-en-translations', function(word) {
-	return EsperantoEnglishDict.find({key: word});
+	var items = EsperantoEnglishDict.find({key: word});
+
+	if (items.count() > 0) {
+		return items;
+	}
+
+	return EsperantoEnglishDict.find({key: word.toLowerCase()});
 });
