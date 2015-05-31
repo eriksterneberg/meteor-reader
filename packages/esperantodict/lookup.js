28999dc2ebd3c637fe0f82bd59d4fbd43d85c52b
Erik Sterneberg <erik.sterneberg@gmail.com>
2015-05-31 17:13:08 +0800

    Added simple stemming of words

--- packages/esperantodict/lookup.js
+++ packages/esperantodict/lookup.js
@@ -1,13 +1,49 @@
 EoEnDict = {};
 
-var getTranslation = function(word) {
-	var items = EsperantoEnglishDict.find({key: word});
 
+var getTranslation = function(word) {
+
+	// Original
+	var items = EsperantoEnglishDict.find({key: word});
 	if (items.count() > 0) {
 		return items;
 	}
 
-	return EsperantoEnglishDict.find({key: word.toLowerCase()});	
-};
+	// Lower case
+	word = word.toLowerCase();
+	items = EsperantoEnglishDict.find({key: word});
+	if (items.count() > 0) {
+		return items;
+	}
 
+	// Simple stemming
+	var words = stem(word);
+	if (words.length > 0) {
+		return EsperantoEnglishDict.find({key: {"$in": words}})
+	}
+};
 EoEnDict.getTranslation = getTranslation;
+
+
+var stem = function(s) {
+	var parts = [];
+
+	for (var j=s.length; j>=0; j--) {
+		var substr = s.substring(0, j);
+		var items = EsperantoEnglishDict.find({key: substr})
+		if (items.count() > 0) {
+			parts.push(substr);
+
+			var tail = '-' + s.substring(j, s.length);
+			items = EsperantoEnglishDict.find({key: tail});
+			if (items.count()) {
+				parts.push(tail);
+			}
+
+			return parts;
+		}
+	}
+
+	return parts;
+};
+EoEnDict.stem = stem;
