453f7fad6b4b7c0f9e7ee06a5733602603374df4
Erik Sterneberg <erik.sterneberg@gmail.com>
2015-06-13 09:22:16 +0800

    Changed font

--- packages/reader/reader.js
+++ packages/reader/reader.js
@@ -11,7 +11,7 @@ var textToParagraphs = function (text) {
 };
 
 
-PARAGRAPHS_INCREMENT = 25;
+PARAGRAPHS_INCREMENT = 10;
 
 var getParagraphsSkip = function (docId) {
 	var skip = Session.get('/' + docId + '/paragraphsSkip/');
